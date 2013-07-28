1b9c024bd6eeaad76a21e31bd9ff3337005300b8
Olanipekun Israel Olufemi <iolufemi@ymail.com>
2013-07-28 21:44:58 +0100

    read me edit stuff

---
+++ db/newtoletlagos (1).sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 28, 2013 at 08:30 PM
+-- Server version: 5.5.21
+-- PHP Version: 5.3.10
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `newtoletlagos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity_idx` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('7c48eb893925d4a09658b4fcabe606ff', '127.0.0.1', 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36', 1375043414, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE IF NOT EXISTS `roles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role`, `description`) VALUES
+(1, 'Administrator', 'This is an adminitrator of the website, he is in charge.'),
+(3, 'Agent', 'This is a Real Estate Agent ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE IF NOT EXISTS `status` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `allow` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `status`
+--
+
+INSERT INTO `status` (`id`, `status`, `description`, `allow`) VALUES
+(1, 'Unverified', 'This is an Unverified user', 0),
+(2, 'Verified', 'This is a Verified User', 1),
+(3, 'Suspended', 'This user is suspended', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `address` text,
+  `password` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT '1',
+  `verificationcode` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `reg_date` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `sex`, `email`, `phone`, `address`, `password`, `role`, `status`, `verificationcode`, `avatar`, `reg_date`) VALUES
+(3, 'admin', 'admin', 'admin', 'male', 'admin@admin.com', '05212458756', 'this is me', '0df3f621b8407583af51cb130141e9df284caecd', '1', '2', '10cdd060b8ca39a0c7da9605568fdb13cd7ffe82', '10cdd060b8ca39a0c7da9605568fdb13cd7ffe82.jpg', '2013-07-28 20:30:03'),
+(6, 'Tunde', 'Falade', 'izzybee', 'male', 'htyj@hfdlo.com', '07056542541', 'rybrwjntuybnsu', '42077af005c73d1cd86701e09958041afe52833d', '3', '2', '8c15423be9e64f611e68df551427f56177e8fa45', NULL, '2013-07-22 07:17:46'),
+(7, 'feni', 'oleje', 'administrator', 'male', 'fsf@fdfeds.com', '1234567890', 'rseftyghjklsrbtr', '42077af005c73d1cd86701e09958041afe52833d', '1', '2', 'dcd412fc43f2fde803c18aa132d97955b130cb16', 'dcd412fc43f2fde803c18aa132d97955b130cb16.jpg', '2013-07-20 23:23:21');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- readme.md
+++ readme.md
@@ -1,7 +1,42 @@
 <html>
 <body>
-<h1><strong>To Let Lagos Property Listing Website</strong></h1>
+<cite>Hello World!</cite>
+<h1><strong>Property Listing App</strong></h1>
 <p>This script is a property listing website for real estate.</p>
 <p><i>This script is not completed yet.</i></p>
+<h2>TO DO:</h2>
+<p>
+toletlg modules
+=============
+
+users - done<br />
+avatars - done<br />
+user search - done<br />
+suspended-done<br />
+roles - done<br />
+status -done<br />
+user access control - done<br /><br />
+
+properties<br />
+property type<br />
+locations<br />
+list type<br />
+property images<br />
+property status<br />
+messages<br />
+recommend property to a friend<br />
+
+</p>
+<b> get the db in the db folder, edit db settings in application/config/database
+login with username: admin password:password123 </b>
+<p>
+Rules:</br>
+one db:table to a module eg, a table named foo must have a module named foo</br>
+all module that has a db must have cruds where s is search</br>
+please, use the template module provied to render view files. you can improve on it tho.
+
+</p>
+<cite>Code is Poetry.</cite>
+<cite>May the force be with all.</cite>
 </body>
 </html>
